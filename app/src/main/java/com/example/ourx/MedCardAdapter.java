38dd9c04fb24c86db0e4c3ea0f27c5afa704a826
kcheney11 <kate.cheney72@gmail.com>
2019-04-08 13:40:00 -0400

    Medicine card turns red if past time to take

--- app/src/main/java/com/example/ourx/MedCardAdapter.java
+++ app/src/main/java/com/example/ourx/MedCardAdapter.java
@@ -1,6 +1,7 @@
 package com.example.ourx;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,19 +9,23 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /* Source: https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView */
 public class MedCardAdapter extends ArrayAdapter<MedicineCard> {
 
     private String timeString;
+    private boolean needToCheckHour;
 
     public MedCardAdapter(Context context, ArrayList<MedicineCard> users, boolean isPast) {
         super(context, 0, users);
         if (isPast) {
-            timeString = "Taken yesterday at ";
+            timeString = "Taken today at ";
         } else {
             timeString = "Take today at ";
         }
+
+        needToCheckHour = isPast;
     }
 
     @Override
@@ -42,8 +47,43 @@ public class MedCardAdapter extends ArrayAdapter<MedicineCard> {
         medTime.setText(finalString);
 
         /* TODO if time to take after current time outline red */
+        Calendar rightNow = Calendar.getInstance();
+        int currentHour = rightNow.get(Calendar.HOUR_OF_DAY);
+        int currentMin = rightNow.get(Calendar.MINUTE);
+        int medHour = parseHour(medicineCard.getTimeToTake());
+        int medMinute = parseMinute(medicineCard.getTimeToTake());
+
+
+        /* In past mode, aka need to check the hour */
+        if (needToCheckHour && !medicineCard.isTaken()) {
+            if (currentHour > medHour) {
+                medName.setTextColor(Color.parseColor("#f44253"));
+
+            } else if (currentHour == medHour) {
+                if (currentMin > medMinute) {
+                    medName.setTextColor(Color.parseColor("#f44253"));
+                }
+            }
+        }
 
         // Return the completed view to render on screen
         return convertView;
+    }
+
+    private int parseHour(String time) {
+        int colonIndex = time.indexOf(':');
+        if (colonIndex < 0 || colonIndex > 2) {
+            return -1;
+        }
+
+        else return Integer.parseInt(time.substring(0, colonIndex));
+    }
+
+    private int parseMinute(String time) {
+        int colonIndex = time.indexOf(':');
+        if (colonIndex < 0 || colonIndex > 2) {
+            return -1;
+        }
+        else return Integer.parseInt(time.substring(colonIndex + 1));
     }
 }
