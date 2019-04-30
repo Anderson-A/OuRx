7fc96153c64cc579a1611cc71d411c2229ec3398
rachel-rosset <rrosset1@jhu.edu>
2019-04-29 20:30:49 -0400

    Merged into master

--- app/src/main/java/com/example/ourx/MedicineDao.java
+++ app/src/main/java/com/example/ourx/MedicineDao.java
@@ -7,6 +7,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 /* templated from https://codelabs.developers.google.com/codelabs/android-room-with-a-view/
  * Step 5: Create the DAO */
@@ -18,6 +19,12 @@ public interface MedicineDao {
 
     @Query("SELECT * FROM medicineentity WHERE med_name = :name")
     MedicineEntity getMedByName(String name);
+
+    //@Query("UPDATE medicineentity SET med_taken=:taken WHERE mid = :id")
+    //void update(int id, String taken);
+
+    @Update
+    void update(MedicineEntity... meds);
 
     @Insert
     void insert(MedicineEntity medicineEntity);
